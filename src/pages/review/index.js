0e08399a27c7c5dea68cddd19b35950e03ab3345
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2018-09-17 23:52:39 +0200

    install query string module and get query id on review page

--- src/pages/review/index.js
+++ src/pages/review/index.js
@@ -1,1 +1,7 @@
 import 'styles/main.scss';
+import queryString from 'query-string';
+
+const query = queryString.parse(window.location.search);
+const reviewId = query && query.id;
+
+console.log(reviewId)
