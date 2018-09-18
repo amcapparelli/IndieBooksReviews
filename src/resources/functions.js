5cbb7702e15d2684a516f84eb504537b2a5a20bc
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2018-09-18 22:38:09 +0200

    calculate date of publish and show it in mminutes

--- src/resources/functions.js
+++ src/resources/functions.js
@@ -27,10 +27,32 @@ export function showResults (data, parentDiv) {
         } else if (key === 'cover' ){
             value = data[key]
             createNodes(parentDiv, 'img', value)
-        } else if (key === 'author' || key === 'reviewer' || key === 'date' ) {
+        } else if (key === 'date') {
+            value = document.createTextNode(calculateDate(data[key]))
+            createNodes(parentDiv, 'p', value)
+        }
+        else if (key === 'author' || key === 'reviewer' ) {
             createNodes(parentDiv, 'p', value)
         }
     }
+}
+
+function calculateDate(dateReviewPublished) {
+    let valueDate = ''
+    const date = new Date()
+    const currentTime = date.getTime()
+    const datePublished = new Date (dateReviewPublished)
+    const datePublishedDD = datePublished.getDate() 
+    const datePublishedMM = datePublished.getMonth() 
+    const datePublishedYY = datePublished.getFullYear() 
+    const diff = (currentTime - datePublished)
+    const diffMins = Math.round(diff/60000)
+    if (diffMins < 1440) {
+        valueDate = 'publicado hace '+ diffMins+ ' minutos'
+    } else {
+        valueDate = 'publicado el '+ datePublishedDD+ '/'+ datePublishedMM+ '/'+datePublishedYY
+    }
+    return valueDate
 }
 
 export default {
