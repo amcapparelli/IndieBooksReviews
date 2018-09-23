c025ab1044190bff484fb1e8d3777b6aac829988
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2018-09-23 13:40:02 +0200

    divide long functions and add comments to the code

--- src/resources/functions.js
+++ src/resources/functions.js
@@ -2,37 +2,48 @@ import ConnectDB from 'database/conn';
 import  avatar  from 'resources/img/avatar.png';
 import { YoutubePlayerFunc } from 'resources/utils';
 
+//Select avatar from DB or default avatar
+const selectAvatar = (textNode, parentElement) => {
+    if (textNode !== '') {
+        parentElement.src = textNode
+        parentElement.classList.add ('avatar')
+    } else {
+        parentElement.src = avatar
+        parentElement.classList.add ('avatar')
+    }
+}
+
+//Add readMore link on each review
+const readMoreLink = (id, textNode, parentElement) => {
+    let leeMas = document.createElement('p')
+    leeMas.id = 'readmore-link'
+    leeMas.innerHTML = `<a href="./review?id=${id}" > Leer artículo completo --> </a> <br>
+                        <a href="./review?id=${id}#comment-area" > Leer comentarios --> </a>`
+    let extracto = document.createElement('span')
+    extracto.innerHTML = textNode
+    parentElement.appendChild(leeMas, extracto)
+    parentElement.insertBefore(extracto, leeMas)
+}
+
+//Create each element of the reviews list
 export function createNodes (container, parent, textNode, id) {
     let parentElement = document.createElement(parent)
     container.appendChild(parentElement)
     if ( parent === 'img' && id !== undefined ) {
-        if (textNode !== '') {
-            parentElement.src = textNode
-            parentElement.classList.add ('avatar')
-        } else {
-            parentElement.src = avatar
-            parentElement.classList.add ('avatar')
-        }
+        selectAvatar(textNode, parentElement)
     } else if ( parent === 'img' ) {
         parentElement.src = textNode
     } else if ( parent === 'div' ){
         parentElement.id = 'ytplayer'+id
         YoutubePlayerFunc(parentElement, parentElement.id, textNode)
     } else if ( parent === 'article') {
-        let leeMas = document.createElement('p')
-        leeMas.id = 'readmore-link'
-        leeMas.innerHTML = `<a href="./review?id=${id}" > Leer artículo completo --> </a> <br>
-                            <a href="./review?id=${id}#comment-area" > Leer comentarios --> </a>`
-        let extracto = document.createElement('span')
-        extracto.innerHTML = textNode
-        parentElement.appendChild(leeMas, extracto)
-        parentElement.insertBefore(extracto, leeMas)
-    }
-    else {
+        readMoreLink(id, textNode, parentElement)
+    } else {
         parentElement.appendChild(textNode)
     }
 }
 
+// Get Results from DB and create a text node with each one
 export function showResults (data, parentDiv) {
     let id = data.id
     for (let key in data) {
@@ -64,17 +75,9 @@ export function showResults (data, parentDiv) {
     }
 }
 
-export function calculateDate(dateReviewPublished) {
+// calculate how long ago the review was published
+const calculateDiffDate = (diffMins, diffHours, datePublishedDD, datePublishedMM, datePublishedYY) => {
     let valueDate = ''
-    const date = new Date()
-    const currentTime = date.getTime()
-    const datePublished = new Date (dateReviewPublished)
-    const datePublishedDD = datePublished.getDate() 
-    const datePublishedMM = datePublished.getMonth() 
-    const datePublishedYY = datePublished.getFullYear() 
-    const diff = (currentTime - datePublished)
-    const diffMins = Math.round(diff/60000)
-    const diffHours = Math.round(diff/3600000)
     if (diffMins < 60 ) {
         valueDate = 'publicado hace '+ diffMins+ ' minutos'
     } else if (diffMins > 60 && diffMins < 1440 ){
@@ -84,10 +87,24 @@ export function calculateDate(dateReviewPublished) {
         valueDate = 'publicado el '+ datePublishedDD+ '/'+ datePublishedMM+ '/'+datePublishedYY
     }
     return valueDate
-    
 }
 
-export const showComments = (comments) => {
+//get actual date and date of reviews
+export function calculateDate(dateReviewPublished) {
+    const date = new Date()
+    const currentTime = date.getTime()
+    const datePublished = new Date (dateReviewPublished)
+    const datePublishedDD = datePublished.getDate() 
+    const datePublishedMM = datePublished.getMonth() 
+    const datePublishedYY = datePublished.getFullYear() 
+    const diff = (currentTime - datePublished)
+    const diffMins = Math.round(diff/60000)
+    const diffHours = Math.round(diff/3600000)
+    return calculateDiffDate(diffMins, diffHours, datePublishedDD, datePublishedMM, datePublishedYY)
+}
+
+//show comments from DB
+const showComments = (comments) => {
     let commentsAreaContanier = document.querySelector('.comment-area')
     if (comments.length !== 0) {
     comments.forEach(comment  => {
@@ -106,6 +123,7 @@ export const showComments = (comments) => {
     }   
 }
 
+//Connect to DB and get comments
 export const getComments = (id) => {
     try {
         let connection = new ConnectDB()
@@ -116,13 +134,11 @@ export const getComments = (id) => {
     } catch (error) {
         console.log('Hubo un error al cargar los comentarios', error)
     }   
-    
 }
 
 export default {
     createNodes,
     showResults,
     calculateDate,
-    getComments,
-    showComments
+    getComments
 }
